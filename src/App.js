29faebb41e24a94562684c4fe19815d8ade0c713
Davi Garms <davigarms@gmail.com>
2020-12-17 00:29:11 +0000

    Add bottom bar with BottomMenuItem component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import BottomMenuItem from './components/BottomMenuItem/BottomMenuItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="bottom-bar" className="bottom-bar">
+        <div className="bottom-menu">
+          <BottomMenuItem id="water-awareness" icon="info" title="Water Awareness"/>
+          <BottomMenuItem  id="find-water" icon="room" title="Find Water" selected={ true }/>
+          <BottomMenuItem  id="updates" icon="notifications" title="Updates"/>
+        </div>
+      </div>
+      <div id="container" className="map-container">
+        <div className="map">
+        </div>
+        <div className="search">
+          <form action>
+            <label htmlFor="fsearch">Search</label>
+            <input type="text" id="fsearch" name="fsearch" placeholder="Search areas" />
+          </form>
+          <div className="search__mylocation material-icons">
+            gps_fixed
+          </div>
+        </div>
+        <div className="map-labels">
+          <div className="map-labels__item station">
+            <div className="map-labels__icon material-icons">room</div>
+            <div className="map-labels__title">
+              Clean Water Station
+            </div>
+          </div>
+          <div className="map-labels__item natural">
+            <div className="map-labels__icon material-icons">room</div>
+            <div className="map-labels__title">
+              Natural Water Source
+            </div>
+          </div>
+          <div className="map-labels__item updates">
+            <div className="map-labels__icon material-icons">info</div>
+            <div className="map-labels__title">
+              Updates
+            </div>
+          </div>
+          <div className="map-labels__item groundwater">
+            <div className="map-labels__icon material-icons">waves</div>
+            <div className="map-labels__title">
+              Groundwater
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/BottomMenuItem/BottomMenuItem.js
@@ -0,0 +1,16 @@
+import './BottomMenuItem.css';
+
+function BottomMenuItem({id, icon, title, selected}) {
+  return (
+    <button data-area="{id}" className={`bottom-menu-item ${selected ? 'selected' : ''}`}>
+        <div className="bottom-menu-item__icon material-icons">
+            <a href="findwater.html">{icon}</a>
+        </div>
+        <div className="bottom-menu-item__title">
+            <a href="findwater.html">{title}</a>
+        </div>
+    </button>
+  );
+}
+
+export default BottomMenuItem;
