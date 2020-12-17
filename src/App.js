c3bb71f60e7e6afdeac727c2084db342e020c46f
Davi Garms <davigarms@gmail.com>
2020-12-17 00:32:42 +0000

    Remove unused elements

--- src/App.js
+++ src/App.js
@@ -11,45 +11,6 @@ function App() {
           <BottomMenuItem  id="updates" icon="notifications" title="Updates"/>
         </div>
       </div>
-      <div id="container" className="map-container">
-        <div className="map">
-        </div>
-        <div className="search">
-          <form action>
-            <label htmlFor="fsearch">Search</label>
-            <input type="text" id="fsearch" name="fsearch" placeholder="Search areas" />
-          </form>
-          <div className="search__mylocation material-icons">
-            gps_fixed
-          </div>
-        </div>
-        <div className="map-labels">
-          <div className="map-labels__item station">
-            <div className="map-labels__icon material-icons">room</div>
-            <div className="map-labels__title">
-              Clean Water Station
-            </div>
-          </div>
-          <div className="map-labels__item natural">
-            <div className="map-labels__icon material-icons">room</div>
-            <div className="map-labels__title">
-              Natural Water Source
-            </div>
-          </div>
-          <div className="map-labels__item updates">
-            <div className="map-labels__icon material-icons">info</div>
-            <div className="map-labels__title">
-              Updates
-            </div>
-          </div>
-          <div className="map-labels__item groundwater">
-            <div className="map-labels__icon material-icons">waves</div>
-            <div className="map-labels__title">
-              Groundwater
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
